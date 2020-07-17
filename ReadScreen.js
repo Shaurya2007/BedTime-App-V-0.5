73245e1d351d8efe05324d952f1ffcfdbdf1f96c
Shaurya2007 <57561094+Shaurya2007@users.noreply.github.com>
2020-07-17 09:30:20 +0530

    Add files via upload

---
+++ ReadScreen.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import {View,Text,TouchableOpacity,TextInput} from 'react-native'
+
+export default class ReadScreen extends Component{
+render(){
+    return(
+        <View>
+            <Text>ReadScreen</Text>
+        </View>
+    )
+}
+}
